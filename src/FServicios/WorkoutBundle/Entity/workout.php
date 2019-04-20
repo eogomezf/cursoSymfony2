31b154b4c8ccfa22894eb280d38cad3ef77f5bb9
Elvis Gomez <eogomezf@gmail.com>
2019-04-20 15:32:08 -0600

    Creacion de entidad

---
+++ src/FServicios/WorkoutBundle/Entity/workout.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace FServicios\WorkoutBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * workout
+ *
+ * @ORM\Table()
+ * @ORM\Entity(repositoryClass="FServicios\WorkoutBundle\Entity\workoutRepository")
+ */
+class workout
+{
+    /**
+     * @var integer
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="activity", type="string", length=255)
+     */
+    private $activity;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="occurrenceDate", type="date")
+     */
+    private $occurrenceDate;
+
+
+    /**
+     * Get id
+     *
+     * @return integer 
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set activity
+     *
+     * @param string $activity
+     * @return workout
+     */
+    public function setActivity($activity)
+    {
+        $this->activity = $activity;
+    
+        return $this;
+    }
+
+    /**
+     * Get activity
+     *
+     * @return string 
+     */
+    public function getActivity()
+    {
+        return $this->activity;
+    }
+
+    /**
+     * Set occurrenceDate
+     *
+     * @param \DateTime $occurrenceDate
+     * @return workout
+     */
+    public function setOccurrenceDate($occurrenceDate)
+    {
+        $this->occurrenceDate = $occurrenceDate;
+    
+        return $this;
+    }
+
+    /**
+     * Get occurrenceDate
+     *
+     * @return \DateTime 
+     */
+    public function getOccurrenceDate()
+    {
+        return $this->occurrenceDate;
+    }
+}
